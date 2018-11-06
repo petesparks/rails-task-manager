aa9e933d66e3a8b8a3d8166c7a3ff7ec0b40eb78
Peter Sparks <petersparks18@gmail.com>
2018-11-06 14:16:17 +0000

    task manager logic complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tasks = Task.create([{title: 'Insurance', details: 'Get a better quote'}, {title: 'Flights', details: 'Check Ryan Air'}])
